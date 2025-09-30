4f6c1c8476fb2e98975e2f8c0b8e7b167207e856
Masri-Programmer <masri_mohamad@protonmail.com>
2025-09-30 17:38:25 +0200

    Cors logs fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://testing.piepjack-clothing.de',
+        'https://piepjack-clothing.de',
+        'http://localhost:5173',
+        '*',
+    ],
 
     'allowed_origins_patterns' => [],
 
