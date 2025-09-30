55d742833593a7a9c1a2660c8de528c6a42159fd
Masri-Programmer <masri_mohamad@protonmail.com>
2025-09-30 22:14:21 +0200

    tab name

--- resources/js/router.js
+++ resources/js/router.js
@@ -12,7 +12,7 @@ import PublicNotFoundLayout from "./layouts/shop/NotFound.vue";
 import { useArea } from "@lib/useArea.js";
 import { setAppArea } from "@lib/useArea";
 const { updateArea } = useArea();
-const BASE_TITLE = "PIEPJACK CLOTHING";
+const BASE_TITLE = env("APP_NAME");
 
 const isAuthenticated = () => {
     const userLocal = useStorage("user", {});
